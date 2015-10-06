fb5cdd271d063c32a67d685935d6af04d56f6add
whats0n <mrdvv@mail.ru>
2015-10-06 11:31:03 +0300

    upd

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,5 @@
 //= lib/jquery.js
 //= lib/slick.min.js
+//= lib/bootstrap.min.js
+//= lib/bootstrap-slider.min.js
 //= common.js
--- src/js/common.js
+++ src/js/common.js
@@ -15,5 +15,63 @@ $(document).ready(function() {
 	//     scrollFixedElements()
 	// });
 
-	console.log($('body').html());
+	$('.js-range').each(function() {
+
+		var range = $(this),
+			rangeText = range.data('text');
+
+
+
+		range.slider({
+			tooltip: 'always',
+			formatter: function(value) {
+				return value + rangeText;
+			}
+		})
+		.on('slideStop', function() {			
+
+			var	tooltipBox = $(this).parents('.js-range-parent').find('.tooltip'),
+				handle = $(this).parents('.js-range-parent').find('.slider-handle'),
+				track = $(this).parents('.js-range-parent').find('.slider-track');
+
+			if (track.position().left >= (handle.position().left - handle.outerWidth())) {
+				tooltipBox.css('margin-left', track.position().left - handle.position().left - handle.outerWidth()/2);
+			};
+			if (track.outerWidth() <= (handle.position().left + handle.outerWidth())) {
+				tooltipBox.css('margin-left', track.outerWidth() - tooltipBox.position().left - tooltipBox.outerWidth() + handle.outerWidth()/2);
+			};
+		})
+		.on('slide', function() {			
+
+			var	tooltipBox = $(this).parents('.js-range-parent').find('.tooltip'),
+				handle = $(this).parents('.js-range-parent').find('.slider-handle'),
+				track = $(this).parents('.js-range-parent').find('.slider-track');
+
+			if (track.position().left >= (handle.position().left - handle.outerWidth())) {
+				tooltipBox.css('margin-left', track.position().left - handle.position().left - handle.outerWidth()/2);
+			};
+			if (track.outerWidth() <= (handle.position().left + handle.outerWidth())) {
+				tooltipBox.css('margin-left', track.outerWidth() - tooltipBox.position().left - tooltipBox.outerWidth() + handle.outerWidth()/2);
+			};
+		});
+	});
+
+
+	
+	$('.js-nav').each(function() {
+		var nav = $(this),
+			navBtn = $('.js-nav-btn');
+
+		navBtn.click(function() {
+			nav.toggleClass('is-active');
+			return false;
+		});
+		$('body').click(function() {			
+			nav.removeClass('is-active');
+		});
+		nav.click(function(event) {
+			event.stopPropagation();
+		});
+	});
+
 });
